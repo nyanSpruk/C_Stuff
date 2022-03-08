aea22a3289896b2757de0c167560d6ccd90eb6bd
Slimenian <slimenian@gmail.com>
2022-03-08 11:20:46 +0900

    Started vaje2

---
+++ Vaje/vaje02/trojice/trojice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
