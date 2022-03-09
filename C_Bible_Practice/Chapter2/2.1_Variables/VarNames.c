4652ae5962561e3b37b50501e9a6d2a34b33da4f
Slimenian <slimenian@gmail.com>
2022-03-09 15:45:48 +0900

    Started Chapter2 - 2.1 variable names

---
+++ C_Bible_Practice/Chapter2/2.1_Variables/VarNames.c
@@ -0,0 +1,2 @@
+// Do not start variable names with _ becuase they are usually resevered for library routines
+// In C we usually use lowercase variables for normal ones and all uppercase for constants
