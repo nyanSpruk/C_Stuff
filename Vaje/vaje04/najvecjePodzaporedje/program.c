12898a9655fdcb01cbdae3713f09d6e310598b50
Slimenian <slimenian@gmail.com>
2022-03-21 19:07:25 +0900

    Optimised the code and finished vaje4

--- Vaje/vaje04/najvecjePodzaporedje/program.c
+++ Vaje/vaje04/najvecjePodzaporedje/program.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main(int argc, char const *argv[])
 {
     int n;
     scanf("%d", &n);
-    int arr[n];
 
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
+    int max = INT_MIN;
     int vsota = 0;
-    int max = 0;
-    max = arr[0];
+
     for (int i = 0; i < n; i++)
     {
-        vsota = 0;
-        for (int j = i; j < n; j++)
-        {
-            vsota += arr[j];
-            if (vsota > max)
-                max = vsota;
-        }
+        int temp;
+        scanf("%d", &temp);
+        vsota += temp;
+        if (vsota > max)
+            max = vsota;
+        if (vsota < 0)
+            vsota = 0;
     }
     printf("%d\n", max);
 
