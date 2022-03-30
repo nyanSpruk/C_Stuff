06b3a790528d30a51649b49c0962ac39a902815a
Slimenian <slimenian@gmail.com>
2022-03-30 13:38:12 +0900

    Added vaje05

---
+++ Vaje/vaje05/naloga.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "naloga.h"
+
+int vsota(int* zac, int* kon) {
+    // popravite / dopolnite ...
+    return -1;
+}
+
+void indeksInKazalec(int* t, int* indeks, int** kazalec) {
+    // dopolnite ...
+}
+
+void frekvenceCrk(char* niz, int** frekvence) {
+    // dopolnite ...
+}
+
+int main() {
+    // koda za ro"cno testiranje (po "zelji)
+
+    return 0;
+}
