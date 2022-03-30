18e93259cec972177458e31e06874a80069b3372
Slimenian <slimenian@gmail.com>
2022-03-30 15:51:51 +0900

    Finished vaje 5

--- Vaje/vaje05/naloga.c
+++ Vaje/vaje05/naloga.c
@@ -4,21 +4,56 @@
 
 #include "naloga.h"
 
-int vsota(int* zac, int* kon) {
-    // popravite / dopolnite ...
-    return -1;
+int vsota(int *zac, int *kon)
+{
+    int vsota = 0;
+    while (*zac != *kon)
+    {
+        vsota += *zac;
+        zac++;
+    }
+    return vsota;
 }
 
-void indeksInKazalec(int* t, int* indeks, int** kazalec) {
-    // dopolnite ...
+void indeksInKazalec(int *t, int *indeks, int **kazalec)
+{
+    if (*indeks == -1)
+    {
+        *indeks = *kazalec - t;
+    }
+    else
+    {
+        *kazalec = t + *indeks;
+    }
 }
 
-void frekvenceCrk(char* niz, int** frekvence) {
-    // dopolnite ...
+void frekvenceCrk(char *niz, int **frekvence)
+{
+    char *p = niz;
+    int *pogostosti = calloc(26, sizeof(int));
+    while (*p != '\0')
+    {
+        char znak = *p;
+        if ((znak >= 'A' && znak <= 'Z') || (znak >= 'a' && znak <= 'z'))
+        {
+            int indeks = (znak >= 'A') ? znak - 'A' : znak - 'a';
+            // pogostosti[indeks]++;
+            *(pogostosti + indeks) += 1;
+        }
+        p++;
+    }
+    *frekvence = pogostosti;
 }
 
-int main() {
+int main()
+{
     // koda za ro"cno testiranje (po "zelji)
+    // int *t = malloc(10 * sizeof(int));
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     *(t + i) = i + 1;
+    // }
 
+    // vsota(t, t + 9);
     return 0;
 }
