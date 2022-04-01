f963cb59b74c3d08872a31cec0c7373772c68139
Slimenian <slimenian@gmail.com>
2022-04-02 07:39:33 +0900

    Added base files

---
+++ Homework/HW5/naloga2/naloga2.c
@@ -0,0 +1,25 @@
+
+/*
+
+Prevajanje in poganjanje skupaj z datoteko test01.c:
+
+gcc -e__main__ -o test01 test01.c naloga2.c
+./test01
+
+*/
+
+#include <stdio.h>
+#include "naloga2.h"
+
+void zamenjaj(int** a, int** b) {
+    // dopolnite ...
+}
+
+void uredi(int** a, int** b, int** c) {
+    // dopolnite ...
+}
+
+int main() {
+    // koda za ro"cno testiranje (po "zelji)
+    return 0;
+}
