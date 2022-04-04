f0b6358dd2c4067462426d0b67ab8ed6635e7c90
Slimenian <slimenian@gmail.com>
2022-04-04 14:29:00 +0900

    Finished HW5

--- Homework/HW5/naloga1/naloga1.c
+++ Homework/HW5/naloga1/naloga1.c
@@ -13,12 +13,35 @@ gcc -e__main__ -o test01 test01.c naloga1.c
 
 #include "naloga1.h"
 
-int* poisci(int* t, int* dolzina, int** konec) {
-    // popravite / dopolnite ...
-    return NULL;
+int *poisci(int *t, int *dolzina, int **konec)
+{
+    int *prvi = t;
+    int *zadnji = t;
+    int counter = 0;
+    while (*zadnji != 0)
+    {
+        counter++;
+        zadnji++;
+    }
+    while (*prvi != 0)
+    {
+        counter++;
+        prvi--;
+    }
+    *dolzina = counter - 1;
+    *konec = zadnji - 1;
+    prvi = prvi + 1;
+    return prvi;
 }
 
-int main() {
+int main()
+{
+    // int tabela[] = {0, 3, 5, 2, 7, 1, -6, -4, 7, 2, -6, 9, -9, 0};
+    // int n = 0;
+    // int *pk = NULL;
+    // int *pz = poisci(tabela + 12, &n, &pk);
+    // printf("n = %d, *pz = %d, *pk = %d\n", n, *pz, *pk);
+
     // koda za ro"cno testiranje (po "zelji)
     return 0;
 }
--- Homework/HW5/naloga2/naloga2.c
+++ Homework/HW5/naloga2/naloga2.c
@@ -11,15 +11,36 @@ gcc -e__main__ -o test01 test01.c naloga2.c
 #include <stdio.h>
 #include "naloga2.h"
 
-void zamenjaj(int** a, int** b) {
-    // dopolnite ...
+void zamenjaj(int **a, int **b)
+{
+    int *temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void uredi(int** a, int** b, int** c) {
-    // dopolnite ...
+void uredi(int **a, int **b, int **c)
+{
+    if (*(*a) > *(*c))
+        zamenjaj(a, c);
+    if (*(*a) > *(*b))
+        zamenjaj(a, b);
+    if (*(*b) > *(*c))
+        zamenjaj(b, c);
 }
 
-int main() {
-    // koda za ro"cno testiranje (po "zelji)
+int main()
+{
+    // int a = 5;
+    // int b = 3;
+    // int c = 2;
+
+    // int *pa = &a;
+    // int *pb = &b;
+    // int *pc = &c;
+
+    // uredi(&pa, &pb, &pc);
+
+    // printf("*pa = %d, *pb = %d, *pc = %d\n", *pa, *pb, *pc);
+    // printf("a = %d, b = %d, c = %d\n", a, b, c);
     return 0;
 }
