1a310b7c2d85c7597f137b7cc4f16abb604aef10
Slimenian <slimenian@gmail.com>
2022-04-12 20:27:52 +0900

    Finished HW 6

--- Homework/HW6/naloga1/naloga1.c
+++ Homework/HW6/naloga1/naloga1.c
@@ -10,34 +10,38 @@ char *zdruzi(char **nizi, char *locilo)
 {
     char **p = nizi;
     char *niz = malloc(0);
+
+    int sizeOfLocilo = strlen(locilo);
     while (*p != NULL)
     {
         int sizeOfNiz = strlen(*p);
-        int sizeOfN = strlen(niz);
+        int sizeOfWholeNiz = strlen(niz);
+
         if (*(p + 1) == NULL)
         {
-            niz = (char *)realloc(niz, sizeOfNiz + sizeOfN);
+            niz = (char *)realloc(niz, sizeOfNiz + sizeOfWholeNiz);
             strcat(niz, *p);
         }
         else
         {
-            int sizeOf = strlen(locilo);
-            niz = (char *)realloc(niz, sizeOf + sizeOfNiz + sizeOfN);
+            niz = (char *)realloc(niz, sizeOfLocilo + sizeOfNiz + sizeOfWholeNiz);
             strcat(niz, *p);
             strcat(niz, locilo);
         }
+
         p++;
     }
     return niz;
 }
 
-char *NIZI[] = {"abc", "ghi", "", NULL};
+// char *NIZI[] = {"", "", "", "", "", "", NULL};
+// char *NIZI[] = {"", "123", "4567", "8901", "2", "34", "", "567890", NULL};
 
 int main()
 {
-    char *niz = zdruzi(NIZI, "def");
-    printf("<%s>\n", niz);
+    // char *niz = zdruzi(NIZI, "");
+    // printf("<%s>\n", niz);
 
-    exit(0);
+    // exit(0);
     return 0;
 }
--- Homework/HW6/naloga2/naloga2.c
+++ Homework/HW6/naloga2/naloga2.c
@@ -8,18 +8,71 @@
 
 int **ap2pp(int (*kazalec)[N], int izvornoStVrstic, int ciljnoStVrstic)
 {
+    int stElVVrstici = sizeof(*kazalec) / sizeof(int);
+    int stElementov = stElVVrstici * izvornoStVrstic;
+    int stElPerVrstica = stElementov / ciljnoStVrstic;
+    int **table = calloc(ciljnoStVrstic, sizeof(int *));
 
-    return NULL;
+    for (int i = 0; i < ciljnoStVrstic; i++)
+    {
+        *(table + i) = calloc(stElPerVrstica + 1, sizeof(int));
+        for (int j = 0; j < stElPerVrstica; j++)
+        {
+            int temp = *(*(kazalec) + j + stElPerVrstica * i);
+            *(*(table + i) + j) = temp;
+        }
+    }
+    return table;
 }
 
 int (*pp2ap(int **kazalec, int izvornoStVrstic, int *ciljnoStVrstic))[N]
 {
-    // popravite / dopolnite
-    return NULL;
+    int stEl = 0;
+    for (int i = 0; i < izvornoStVrstic; i++)
+    {
+        int *vrstica = *(kazalec + i);
+        for (int j = 0; *(vrstica + j) != 0; j++)
+            stEl++;
+    }
+    *(ciljnoStVrstic) = stEl / N + (stEl % N > 0 ? 1 : 0);
+    int(*rez)[N] = calloc((N) * (*ciljnoStVrstic), sizeof(int));
+    int indexI = 0;
+    int indexJ = 0;
+    for (int i = 0; i < (*ciljnoStVrstic); i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (indexI == izvornoStVrstic)
+                return rez;
+
+            if (kazalec[indexI][indexJ] == 0)
+            {
+                if (indexI == izvornoStVrstic - 1)
+                    return rez;
+                else
+                {
+                    indexI++;
+                    indexJ = 0;
+                }
+            }
+            if (kazalec[indexI][indexJ] != 0)
+            {
+                rez[i][j] = kazalec[indexI][indexJ];
+                indexJ++;
+            }
+            else
+            {
+                indexI++;
+                indexJ = 0;
+                j--;
+            }
+        }
+    }
+
+    return rez;
 }
 
 int main()
 {
-    // koda za ro"cno testiranje (po "zelji)
     return 0;
 }
