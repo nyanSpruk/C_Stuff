9bd4eda6809f8dea90a197005148eb8d88af9e77
Slimenian <slimenian@gmail.com>
2022-05-20 14:09:26 +0900

    Finished hw10

--- Homework/HW10/naloga1/test01.c
+++ Homework/HW10/naloga1/test01.c
@@ -5,24 +5,29 @@
 
 #include "naloga1.h"
 
-Vozlisce* ustvari(int* t, int n) {
-    if (n <= 0) {
+Vozlisce *ustvari(int *t, int n)
+{
+    if (n <= 0)
+    {
         return NULL;
     }
-    Vozlisce* v = malloc(sizeof(Vozlisce));
+    Vozlisce *v = malloc(sizeof(Vozlisce));
     v->podatek = *t;
     v->naslednje = ustvari(t + 1, n - 1);
     return v;
 }
 
-Vozlisce** shrani(int* tabela, Vozlisce* seznam, int n, int k, int* dolzina) {
+Vozlisce **shrani(int *tabela, Vozlisce *seznam, int n, int k, int *dolzina)
+{
     *dolzina = n / k;
-    Vozlisce** shramba = malloc(*dolzina * sizeof(Vozlisce*));
-    Vozlisce* v = seznam;
-    Vozlisce** p = shramba;
+    Vozlisce **shramba = malloc(*dolzina * sizeof(Vozlisce *));
+    Vozlisce *v = seznam;
+    Vozlisce **p = shramba;
     int i = 1;
-    while (v != NULL) {
-        if (i % k == 0) {
+    while (v != NULL)
+    {
+        if (i % k == 0)
+        {
             *p++ = v;
         }
         v = v->naslednje;
@@ -31,11 +36,14 @@ Vozlisce** shrani(int* tabela, Vozlisce* seznam, int n, int k, int* dolzina) {
     return shramba;
 }
 
-void izpisi(Vozlisce* v) {
+void izpisi(Vozlisce *v)
+{
     bool prvic = true;
     printf("[");
-    while (v != NULL) {
-        if (!prvic) {
+    while (v != NULL)
+    {
+        if (!prvic)
+        {
             printf(", ");
         }
         prvic = false;
@@ -45,11 +53,13 @@ void izpisi(Vozlisce* v) {
     printf("]\n");
 }
 
-void preveri(Vozlisce** tabela, Vozlisce* seznam, int dolzina) {
+void preveri(Vozlisce **tabela, Vozlisce *seznam, int dolzina)
+{
     int i = 0;
-    Vozlisce* v = seznam;
-    Vozlisce** t = tabela;
-    while (v != NULL && i < dolzina) {
+    Vozlisce *v = seznam;
+    Vozlisce **t = tabela;
+    while (v != NULL && i < dolzina)
+    {
         printf("%d", v == *t);
         v = v->naslednje;
         t++;
@@ -58,21 +68,24 @@ void preveri(Vozlisce** tabela, Vozlisce* seznam, int dolzina) {
     printf("\n");
 }
 
-void pocisti(Vozlisce* v) {
-    if (v != NULL) {
+void pocisti(Vozlisce *v)
+{
+    if (v != NULL)
+    {
         pocisti(v->naslednje);
         free(v);
     }
 }
 
-int __main__() {
+int __main__()
+{
     int tabela[] = {-617};
     int n = sizeof(tabela) / sizeof(int);
     int k = 1;
 
-    Vozlisce* zacetek = ustvari(tabela, n);
+    Vozlisce *zacetek = ustvari(tabela, n);
     int dolzina = 0;
-    Vozlisce** shramba = shrani(tabela, zacetek, n, k, &dolzina);
+    Vozlisce **shramba = shrani(tabela, zacetek, n, k, &dolzina);
 
     zacetek = zdesetkaj(zacetek, k);
     izpisi(zacetek);
--- Homework/HW10/naloga2/naloga2.c
+++ Homework/HW10/naloga2/naloga2.c
@@ -6,12 +6,78 @@
 
 #include "naloga2.h"
 
-int izpisiA(A* a, char* cilj) {
-    // popravite / dopolnite ...
-    return -9999;
+char *cleanUP[10000];
+int ind = 0;
+
+int izpisiA(A *a, char *cilj);
+char *izpisiA2(A *a);
+char *izpisiB(B *b);
+char *izpisiC(C *c);
+
+int izpisiA(A *a, char *cilj)
+{
+    if (a == NULL)
+    {
+        strcpy(cilj, "NULL");
+        return 4;
+    }
+    int dolzina = sprintf(cilj, "{%d, %s}", a->p, izpisiB(a->b));
+    return dolzina;
 }
 
-int main() {
+char *izpisiA2(A *a)
+{
+    if (a == NULL)
+    {
+        char *temp = malloc(sizeof(char) * 5);
+        strcpy(temp, "NULL");
+        cleanUP[ind] = temp;
+        ind += 1;
+        return temp;
+    }
+    char *temp = malloc(sizeof(char) * 10000);
+    cleanUP[ind] = temp;
+    ind += 1;
+    sprintf(temp, "{%d, %s}", a->p, izpisiB(a->b));
+    return temp;
+}
+
+char *izpisiB(B *b)
+{
+    if (b == NULL)
+    {
+        char *temp = malloc(sizeof(char) * 5);
+        strcpy(temp, "NULL");
+        cleanUP[ind] = temp;
+        ind += 1;
+        return temp;
+    }
+    char *temp = malloc(sizeof(char) * 10000);
+    cleanUP[ind] = temp;
+    ind += 1;
+    sprintf(temp, "{%s, %s}", b->q, izpisiC(b->c));
+    return temp;
+}
+
+char *izpisiC(C *c)
+{
+    if (c == NULL)
+    {
+        char *temp = malloc(sizeof(char) * 5);
+        strcpy(temp, "NULL");
+        cleanUP[ind] = temp;
+        ind += 1;
+        return temp;
+    }
+    char *temp = malloc(sizeof(char) * 10000);
+    cleanUP[ind] = temp;
+    ind += 1;
+    sprintf(temp, "{%s, %s, %s}", c->r ? "true" : "false", izpisiA2(c->a), izpisiB(c->b));
+    return temp;
+}
+
+int main()
+{
     // koda za ro"cno testiranje (po "zelji)
     return 0;
 }
