d14a405e6fec250105c15db246958fe2dcbb8af5
Slimenian <slimenian@gmail.com>
2022-08-27 13:22:27 +0200

    Vaje 9

--- 5Rok/Vaje/vaje09/naloga.c
+++ 5Rok/Vaje/vaje09/naloga.c
@@ -8,15 +8,18 @@
 int poisciStudenta(Student **studentje, int stStudentov, int vpisna)
 {
     for (int i = 0; i < stStudentov; i++)
+    {
         if (studentje[i]->vpisna == vpisna)
             return i;
+    }
 
     return -1;
 }
 
 int poisciPO(Student *student, char *predmet)
 {
-    for (int i = 0; i < student->stPO; i++)
+    int stPO = student->stPO;
+    for (int i = 0; i < stPO; i++)
     {
         if (strcmp(student->po[i].predmet, predmet) == 0)
             return i;
@@ -27,32 +30,31 @@ int poisciPO(Student *student, char *predmet)
 
 int dodaj(Student **studentje, int stStudentov, int vpisna, char *predmet, int ocena)
 {
-    int ix = poisciStudenta(studentje, stStudentov, vpisna);
-
-    if (ix == -1)
+    int indexStudenta = poisciStudenta(studentje, stStudentov, vpisna);
+    if (indexStudenta != -1)
     {
-        Student *temp = malloc(sizeof(Student));
-        temp->vpisna = vpisna;
-        temp->stPO = 1;
-        temp->po = malloc(10 * sizeof(PO));
-        temp->po[0].ocena = ocena;
-        strcpy(temp->po[0].predmet, predmet);
-        studentje[stStudentov] = temp;
-        stStudentov++;
-    }
-    else
-    {
-        int ixPo = poisciPO(studentje[ix], predmet);
-        if (ixPo == -1)
+        int indexPredmeta = poisciPO(studentje[indexStudenta], predmet);
+        if (indexPredmeta != -1)
         {
-            studentje[ix]->po[studentje[ix]->stPO].ocena = ocena;
-            strcpy(studentje[ix]->po[studentje[ix]->stPO].predmet, predmet);
-            studentje[ix]->stPO++;
+            studentje[indexStudenta]->po[indexPredmeta].ocena = ocena;
         }
         else
         {
-            studentje[ix]->po[ixPo].ocena = ocena;
+            studentje[indexStudenta]->po[studentje[indexStudenta]->stPO].ocena = ocena;
+            strcpy(studentje[indexStudenta]->po[studentje[indexStudenta]->stPO].predmet, predmet);
+            studentje[indexStudenta]->stPO++;
         }
+    }
+    else
+    {
+        Student *novS = malloc(1 * sizeof(Student));
+        novS->vpisna = vpisna;
+        novS->stPO = 1;
+        novS->po = malloc(10 * sizeof(PO));
+        novS->po[0].ocena = ocena;
+        strcpy(novS->po[0].predmet, predmet);
+        studentje[stStudentov] = novS;
+        stStudentov++;
     }
     return stStudentov;
 }
